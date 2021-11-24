ce3a403c0cc3aa7cf15cc32d498574b9e4a5c98d
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2021-11-24 22:08:33 +0900

    ADD: Toggle Review Menu

---
+++ src/Components/Review/ReviewInput/styles.ts
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+export const InputWrapper = styled.div`
+  display: flex;
+  background-color: #eee3ff;
+  margin: 0 auto;
+  width: 100%;
+  padding: 0.2rem;
+  border-radius: 5px;
+  overflow: hidden;
+  margin-bottom: 3rem;
+
+  .reviewInput {
+    width: 100%;
+    background-color: inherit;
+    border: none;
+  }
+  .mic {
+    cursor: pointer;
+  }
+`;
