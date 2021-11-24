a80e2e01c02eacbbb6f31187163494b5ed33b4d1
강은빈 <kub0803@naver.com>
2021-11-24 19:50:55 +0900

    [MODIFY] 경로 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
-export { default as Login } from "./login/index";
 export { default as Header } from "./Common/Footer/index";
 export { default as Footer } from "./Common/Footer/index";
+export { default as Login } from "./login/index";
+export { default as SignIn } from "./login/SignIn";
