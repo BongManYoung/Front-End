5f546f289f9d70912b1385cde0e9b6056e9500da
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2021-11-24 17:44:14 +0900

    ADD: Header Component

---
+++ src/Components/Common/Header/HeaderStyles.tsx
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 60px;
+
+  .header {
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 1000px;
+  }
+
+  .logo {
+    width: 60px;
+    height: 60px;
+  }
+
+  .links {
+    display: flex;
+    a {
+      margin-left: 3rem;
+      text-decoration: none;
+      color: black;
+      font-weight: lighter;
+    }
+  }
+`;
--- src/Components/Common/Header/index.tsx
+++ src/Components/Common/Header/index.tsx
@@ -1,5 +1,22 @@
+import { ReactComponent as Logo } from "Assets/MYD_Logo.svg";
+import { HeaderContainer } from "./HeaderStyles";
+import { NavLink } from "react-router-dom";
+
 const Header: React.FC = () => {
-  return <div></div>;
+  return (
+    <HeaderContainer>
+      <div className="header">
+        <Logo className="logo" />
+
+        <nav className="links">
+          <NavLink to="/">오늘의 메뉴</NavLink>
+          <NavLink to="/">이달의 리뷰</NavLink>
+          <NavLink to="/">음성 리뷰</NavLink>
+          <NavLink to="/login">로그인</NavLink>
+        </nav>
+      </div>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
