7e7a72338049175ff564b19e767fd60e385ea786
강은빈 <kub0803@naver.com>
2021-11-25 00:12:35 +0900

    [MODIFY] 메인페이지 스타일 수정

---
+++ src/Components/Mainpage/items/style.ts
@@ -0,0 +1,39 @@
+import styled from "@emotion/styled";
+
+export const ItemBoxWrapper = styled.div`
+  margin: 30px auto;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex: wrap;
+  gap: 30px;
+
+  .item-box {
+    transition: all 0.5s;
+    img {
+      width: 235px;
+      height: 300px;
+      object-fit: cover;
+    }
+
+    .shop-info {
+      display: flex;
+      flex-direction: column;
+
+      span:nth-child(1) {
+        color: #cbcbcb;
+      }
+
+      span:nth-child(3) {
+        font-weight: 500;
+        color: #8fa3ff;
+      }
+    }
+
+    :hover {
+      transform: translateY(-10px);
+    }
+  }
+`;
+
+export const MonthReviewWrapper = styled.div``;
