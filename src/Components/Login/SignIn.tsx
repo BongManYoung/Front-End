67bb6e624f57c39841dfd72ba3b29a2578cfc219
강은빈 <kub0803@naver.com>
2021-11-24 19:50:36 +0900

    [FEAT] 로그인 회원가입

--- src/Components/Login/SignIn.tsx
+++ src/Components/Login/SignIn.tsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import * as S from "./style";
 
-const Login = () => {
+const SignIn = () => {
   const navigate = useNavigate();
   const [buttonColor, setButtonColor] = useState<boolean>(false);
   const [emailBor, setEmailBor] = useState<boolean>(false);
   const [passwordBor, setPasswordBor] = useState<boolean>(false);
+  const [passwordCheckBor, setPasswordCheckBor] = useState<boolean>(false);
   const [inputs, setInputs] = useState({
     nickname: "",
     password: "",
+    passwordCheck: "",
   });
 
-  const { nickname, password } = inputs;
+  const { nickname, password, passwordCheck } = inputs;
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
@@ -26,9 +28,11 @@ const Login = () => {
   useEffect(() => {
     nickname.length >= 2 ? setEmailBor(true) : setEmailBor(false);
     password.length >= 4 ? setPasswordBor(true) : setPasswordBor(false);
-
-    nickname && password ? setButtonColor(true) : setButtonColor(false);
-  }, [nickname, nickname.length, password]);
+    passwordCheck.length >= 4
+      ? setPasswordCheckBor(true)
+      : setPasswordCheckBor(false);
+    passwordCheck && password ? setButtonColor(true) : setButtonColor(false);
+  }, [nickname.length, password, passwordCheck]);
 
   return (
     <S.Wrapper>
@@ -63,17 +67,31 @@ const Login = () => {
               }}
             />
           </div>
+          <div className="input-item-wrap">
+            <input
+              type="password"
+              placeholder="비밀번호를 다시 입력해주세요"
+              name="passwordCheck"
+              value={passwordCheck}
+              onChange={(e) => onChange(e)}
+              style={{
+                borderBottom: passwordCheckBor
+                  ? `2px solid #5271FF`
+                  : `2px solid #c4c4c4`,
+              }}
+            />
+          </div>
         </S.InputWrapper>
         <button
           style={
             buttonColor ? { background: "#5271ff" } : { background: "#c7c7c7" }
           }
         >
-          Login
+          Sign
         </button>
       </S.LoginWrapper>
     </S.Wrapper>
   );
 };
 
-export default Login;
+export default SignIn;
