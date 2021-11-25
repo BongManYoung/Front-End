9d2e456d387f83c8b066ab6697c79ee540a6042e
강은빈 <kub0803@naver.com>
2021-11-25 14:14:45 +0900

    [ADD] 베스트 리뷰 컴포넌트

---
+++ src/Components/Review/ReviewBest/style.ts
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+
+export const BestWrapper = styled.div`
+  width: 100%;
+
+  .best_title {
+    padding: 15px 0;
+    box-sizing: border-box;
+    border-bottom: 1px solid #e0e0e0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    font-size: 18px;
+
+    .title_wrapper {
+      span:nth-child(1) {
+        font-weight: 500;
+      }
+
+      span:nth-child(2) {
+        font-weight: 500;
+        color: #ff3636;
+      }
+    }
+
+    .more_text {
+      color: #868686;
+      font-size: 16px;
+      cursor: pointer;
+    }
+  }
+`;
