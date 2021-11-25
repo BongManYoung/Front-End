d80c549b97c24799341bf7f8b6228616a86a4c9a
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2021-11-25 18:16:37 +0900

    ADD: Chatbot layout

---
+++ src/Components/Chat/ChatInput/styles.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const ChatInputWrapper = styled.div`
+  width: 93%;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 1rem;
+
+  .chat_input {
+    display: flex;
+    width: 95%;
+    background-color: rgba(239, 239, 240, 0.8);
+    border: none;
+    border-radius: 10px;
+    font-size: 1rem;
+    padding: 1rem 10px;
+  }
+`;
